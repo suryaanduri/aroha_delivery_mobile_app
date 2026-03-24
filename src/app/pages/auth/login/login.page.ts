7aab07815e03fad099f3fd296508eb4031f4f115
suryaanduri <suryaanduri@gmail.com>
2026-03-24 11:56:02 +0530

    login ui and theme

--- src/app/pages/auth/login/login.page.ts
+++ src/app/pages/auth/login/login.page.ts
@@ -1,20 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonButton, IonContent, IonIcon } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { eyeOffOutline, eyeOutline, lockClosedOutline, personCircleOutline } from 'ionicons/icons';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonButton, IonContent, IonIcon, CommonModule, FormsModule]
 })
-export class LoginPage implements OnInit {
+export class LoginPage {
+  username = '';
+  password = '';
+  showPassword = false;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
+    addIcons({
+      eyeOffOutline,
+      eyeOutline,
+      lockClosedOutline,
+      personCircleOutline
+    });
   }
 
+  get canSubmit(): boolean {
+    return this.username.trim().length > 0 && this.password.length > 0;
+  }
+
+  togglePasswordVisibility(): void {
+    this.showPassword = !this.showPassword;
+  }
+
+  onSubmit(): void {
+    if (!this.canSubmit) {
+      return;
+    }
+  }
 }
