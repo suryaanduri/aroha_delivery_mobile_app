c553f7276141a45c6d05a0b5f9925e7cf0de91cf
suryaanduri <suryaanduri@gmail.com>
2026-04-14 09:29:41 +0530

    delivery map page changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   apiBaseUrl: 'https://dev.arohafresh.com',
   authPrefix: '/api/auth',
-  googleMapsApiKey: 'AIzaSyCefKODYXsOXw925kEtWC4K0g3Wym_sgCA',
+  googleMapsApiKey: 'AIzaSyCrsj87LulFb6IHGA31UKn58UXMhW9_L_I',
 };
